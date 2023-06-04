6412cfa5f061a6ac66a8c42b382ea8ab35f25f50
Deepak Mecheri <deepakmecheri@gmail.com>
2023-06-05 01:09:54 +0530

    ...

--- 1_Basic_Elements/manim-test.py
+++ 1_Basic_Elements/manim-test.py
@@ -1,7 +1,18 @@
 from manim import *
+from pathlib import Path
+import os
+
+FLAGS = "-pqm -v WARNING"
+SCENE = "FirstScene"
 
 class FirstScene(Scene):
     def construct(self):
         text = Text("Hello World")
         self.play(Write(text))
         self.wait()
+
+
+# This makes it easier to run on vscode. Just hit the play button
+if __name__ == '__main__':
+    script_name = f"{Path(__file__).resolve()}"
+    os.system(f"manim {script_name} {FLAGS} {SCENE}")
